aeef0c8a820affb222313c65599eba6e2f15d460
canbolatkaan <canbolatkaan@gmail.com>
2021-03-14 13:09:52 +0300

    Getting ready for deployment to Heroku

--- src/main/java/MyHomework/App.java
+++ src/main/java/MyHomework/App.java
@@ -4,6 +4,17 @@
 package MyHomework;
 import java.util.ArrayList;
 import java.util.Arrays;
+import static spark.Spark.get;
+  import static spark.Spark.port;
+  import static spark.Spark.post;
+  
+  import java.util.HashMap;
+  import java.util.Map;
+  import spark.ModelAndView;
+  import spark.template.mustache.MustacheTemplateEngine;
+
+
+
 
 public class App {
     public String getGreeting() {
@@ -12,7 +23,71 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          sc1.close();
+          System.out.println(inputList);
+
+          String input2 = req.queryParams("input2");
+          java.util.Scanner sc2 = new java.util.Scanner(input2);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+          while (sc2.hasNext())
+          {
+            int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+            inputList2.add(value);
+          }
+          sc2.close();
+          System.out.println(inputList2);
+
+
+          String input3 = req.queryParams("input3");
+          java.util.Scanner sc3 = new java.util.Scanner(input3);
+          sc3.useDelimiter("[;\r\n]+");
+          int input3AsInt = Integer.parseInt(sc3.next().replaceAll("\\s", ""));
+          int input4AsInt = Integer.parseInt(sc3.next().replaceAll("\\s", ""));
+          System.out.println(input3AsInt +" "+ input4AsInt);
+          boolean result = App.search(inputList, inputList2, input3AsInt,input4AsInt);
+
+          Map<String, Boolean> map = new HashMap<String, Boolean>();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map<String, String> map = new HashMap<String, String>();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+
+
     }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
     public static boolean search(ArrayList<Integer> array1,ArrayList<Integer> array2,int first,int second){
 
@@ -31,7 +106,7 @@ public class App {
 
         if(count == count2 && count == second)
             return true;
-            
+
             return false;
 
     }
